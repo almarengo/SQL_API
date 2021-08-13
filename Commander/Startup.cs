2f6f36688af7f1a2f88fbbbe83f6cb5a054e2786
Alberto Marengo <marengo.albert@gmail.com>
2021-08-12 23:42:39 -0700

    Functional connection to mySQL with Dto

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -38,6 +38,8 @@ namespace Commander
 
             services.AddControllers();
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
 
             //services.AddScoped<ICommanderRepo, MockCommanderRepo>();
